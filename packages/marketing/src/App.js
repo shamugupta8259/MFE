2f9ac929df153903343effa4fac223b9dd854bb6
shamugupta8259 <shamugupta8259@gmail.com>
2026-05-01 15:01:26 +0530

    fixes of classname collosion

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default function App() {
   return (
-    <StylesProvider injectFirst>
+    <StylesProvider generateClassName={generateClassName} >
       <BrowserRouter>
         <Switch>
           <Route path="/pricing" component={Pricing} />
